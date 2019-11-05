76b7b4950762f4d170a32c3dea83a189d54756cd
pabloPeralta <paps21.gol@gmail.com>
2019-11-05 09:19:52 -0600

    ambos jugadores colisionan

--- js/main.js
+++ js/main.js
@@ -1,40 +1,46 @@
 const images = {
-    fondo1: './images/fondo1.png',
-    fondo2: 'https://img.itch.zone/aW1hZ2UvMjE3Mzg5LzEwMjYyODAucG5n/original/cDpnzW.png',
-    fondo3: 'https://ak4.picdn.net/shutterstock/videos/1016737804/thumb/1.jpg',
-    ship1: './images/Ship1.png',
-    ship6: './images/Ship6.png',
-    obst1: './images/obs.png'
+  fondo1: './images/fondo1.png',
+  fondo2: './images/fondo2.jpg',
+  fondo3: 'https://ak4.picdn.net/shutterstock/videos/1016737804/thumb/1.jpg',
+  ship1: './images/Ship1.png',
+  ship6: './images/Ship6.png',
+  obst1: './images/obs.png',
+  bullet:'./images/bullet.png'
 };
+
 //sujetar canvas
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+
 //variables
 let interval;
 let frames = 0;
 let obstacles = [];
 let scoreCount = 0;
+
+//clases
 class Board {
-    constructor(){
-      this.x = 0;
-      this.y = 0;
-      this.width = canvas.width;
-      this.height = canvas.height;
-      //cargar imagen 
-      this.img = new Image();
-      this.img.src = images.fondo3;
-      this.img.onload = () => {
-        this.draw();
-        };
-      }
-    draw(){
-      this.x--; //velocidad fondo
-      //si no tengo el if, la imagen solo se estira, aparece la imagen y se va intercaando con la misma para que no se vea cortada
-      if(this.x < -canvas.width) this.x = 0;
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-      ctx.drawImage(this.img, this.x + canvas.width, this.y, this.width, this.height);
+  constructor(){
+    this.x = 0;
+    this.y = 0;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    //cargar imagen 
+    this.img = new Image();
+    this.img.src = images.fondo2;
+    this.img.onload = () => {
+      this.draw();
+      };
     }
+  draw(){
+    this.x--; //velocidad fondo
+    //si no tengo el if, la imagen solo se estira, aparece la imagen y se va intercaando con la misma para que no se vea cortada
+    if(this.x < -canvas.width) this.x = 0;
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.img, this.x + canvas.width, this.y, this.width, this.height);
   }
+}
 
 
 class Ship {
@@ -44,7 +50,8 @@ class Ship {
     this.height =  100
     //pos
     this.x = 30;
-    this.y = 160;
+    this.y = 0;
+
     //velocidad??
     this.vx = 0
     this.vy = 0
@@ -62,23 +69,20 @@ class Ship {
       this.y = canvas.height - this.height
     }
     //arriba
-    
     else if(this.y <= 0){
       this.y = 0;
     }
-
     //derecha
-    else if (this.x > canvas.width - this.width){
+     if (this.x > canvas.width - this.width){
       this.x = canvas.width - this.width
     }
-
     //izquierda
-   if(this.x < 0){
+   if(this.x <= 0){
       this.x = 0;
     }
-
   ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
   }
+
   isTouching(obstacle) {
     // algo está tratando de ocupar el mismo espacio en canvas que flash
     return (
@@ -115,7 +119,7 @@ class Ship2 {
      this.x = 30;
      this.y = 460;
     
-    this.hp = 1
+    this.hp2 = 1
 
     this.img = new Image()
     this.img.src = images.ship6;
@@ -130,24 +134,19 @@ class Ship2 {
       this.y = canvas.height - this.height
     }
     //arriba
-    
     else if(this.y <= 0){
       this.y = 0;
     }
-
     //derecha
-    else if (this.x > canvas.width - this.width){
+   if (this.x > canvas.width - this.width){
       this.x = canvas.width - this.width
     }
-
     //izquierda
-   if(this.x < 0){
+   else if(this.x <= 0){
       this.x = 0;
     }
     ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
   }
-  
-  
 
   isTouching(obstacle) {
     // algo está tratando de ocupar el mismo espacio en canvas que flash
@@ -187,7 +186,6 @@ class Obs {
     this.img.onload = () => {
       this.draw();
     };
- 
   }
   draw() {
     this.x--;
@@ -195,43 +193,35 @@ class Obs {
   }
 }
 
-
-function clearCanvas(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-}
-
-function checkColitions() {
-  obstacles.forEach((obs, i) => {
-    if (ship.isTouching(obs)) {
-      obstacles.splice(i, 1)
-      ship.hp--
-      gameOver();
-    }
-    if (ship2.isTouching(obs)) {
-      obstacles.splice(i, 1)
-      ship2.hp--
-      gameOver();
-    }
-  })
-}
-function gameOver() {
-  if (ship.hp === 0) {
-    clearInterval(interval)
-    interval= null;
-    ctx.font = '30px Arial'
-    ctx.fillStyle = 'Blue'
-    ctx.fillText('Game Over', canvas.width / 2 - 30, canvas.height / 2)
+class Bullet {
+  constructor(){
+    //pos
+    this.x = ship2.x + 5;
+    this.y = ship2.y;
+    //size
+    this.width = 100
+    this.height =  100
+    this.img = new Image()
+    this.img.src = images.obst1
+    this.img.onload = () => {
+      this.draw();
+    };
   }
-  if(ship2.hp === 0){
-    clearInterval(interval)
-    interval= null;
-    ctx.font = '30px Arial'
-    ctx.fillStyle = 'Red'
-    ctx.fillText('Game Over', canvas.width / 2 - 30, canvas.height / 2)
-  }
+  draw(){
+    this.x--;
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
 }
+
+//clases
+
 function generateObs(){
   if (frames % 200 === 0) { //numero de obs en funcion del tiempo
+    const randomPosition = Math.floor(Math.random() * canvas.height) + 50
+    const obs = new Obs(randomPosition)
+    obstacles.push(obs)
+  }
+  else if(frames % 100 === 0 > 3000 ){
     const randomPosition = Math.floor(Math.random() * canvas.height) + 50
     const obs = new Obs(randomPosition)
     obstacles.push(obs)
@@ -241,75 +231,114 @@ function drawObstacles() {
   generateObs();
   obstacles.forEach(obs => obs.draw())
 }
+
+function checkColitions() {
+  obstacles.forEach((obs, i) => {
+    if (ship.isTouching(obs)) {
+      obstacles.splice(i, 1)
+      ship.hp--
+      gameOver();
+    } 
+    if (ship2.isTouching(obs)) {
+      obstacles.splice(i, 1)
+      ship2.hp2--
+      gameOver();
+    }
+  })
+}
+
+function clearCanvas(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+}
+
+function gameOver() { ///revisar el texto 
+  if (ship.hp === 0) {
+    clearInterval(interval)
+    interval= null;
+    ctx.font = '50px Arial'
+    ctx.fillStyle = 'purple'
+    ctx.fillText('Jugador 2 gana!', canvas.width / 2 - 60, canvas.height / 2)
+  }
+   if(ship2.hp2 === 0){
+    clearInterval(interval)
+    interval= null;
+    ctx.font = '50px Arial'
+    ctx.fillStyle = 'green'
+    ctx.fillText('Jugador 1 gana!', canvas.width / 2 , canvas.height / 2)
+  }
+}
+
 const board = new Board();
 const ship = new Ship();
 const ship2 = new Ship2();
 
+
 window.onload = function() {
-    document.getElementById("start-button").onclick = function() {
-      startGame();
-    };  
-  };
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+  }; 
+  document.getElementById("restart-button").onclick = function(){
+    restart();
+  }
+};
 
 function startGame() {
-    if(interval) return;
-    interval = setInterval(update, 1000 / 90); // /60
+  if(interval) return;
+  interval = setInterval(update, 1000 / 90); // /60
 };
 
 function restart(){
-    /*interval = false;*/
-    ship.x = 30;
-    ship.y  = 70;
-    ship2.x = 30;
-    ship2.y  = 70;
-    frames = 0;
-    obstacles = [];
-    // scoreCount = 0;
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    startGame();
-  }
+  /*interval = false;*/
+  interval = null
+  ship.x = 10;
+  ship.y  = 0;
+  ship2.x = 10;
+  ship2.y  = 550;
+  frames = 0;
+  obstacles = [];
+  // scoreCount = 0;
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  startGame();
+}
 
 document.onkeydown = (e) => {
-    switch(e.keyCode){
-        case 37:
-          ship.moveLeft()
-        return
-        case 38:
-          ship.moveUp()
-        break;
-        case 39:
-          ship.moveRight()
-        return
-        case 40:
-          ship.moveDown()
-        break;
-        case 65:
-          ship2.moveLeft()
-        break;
-        case 68:
-          ship2.moveRight()
-        break;
-        case 82:
-          restart();
-        break;
-        case 83:
-          ship2.moveDown()
-        break;
-        case 87:
-          ship2.moveUp()
-        break;
-        default:
-        break;
-    }
+  switch(e.keyCode){
+      case 37:
+        ship.moveLeft()
+      return
+      case 38:
+        ship.moveUp()
+      break;
+      case 39:
+        ship.moveRight()
+      return
+      case 40:
+        ship.moveDown()
+      break;
+      case 65:
+        ship2.moveLeft()
+      break;
+      case 68:
+        ship2.moveRight()
+      break;
+      case 83:
+        ship2.moveDown()
+      break;
+      case 87:
+        ship2.moveUp()
+      break;
+      default:
+      break;
+  }
 }
 
 function update(){
-    frames++;
-    clearCanvas();
-    board.draw();
-    ship.draw();
-    ship2.draw();
-    checkColitions();
-    drawObstacles();
-    generateObs();
+  frames++;
+  clearCanvas();
+  board.draw();
+  ship.draw();
+  ship2.draw();
+  checkColitions();
+  drawObstacles();
+  generateObs();
 }
